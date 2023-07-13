549f0df06618beefc027f0eb7e62b028c2518752
Haider <haideralirafeeq@gmail.com>
2023-07-13 12:01:13 +0500

    nav remaining

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,50 +1,46 @@
-import React from 'react'
-import "./Header.css"
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import "./Header.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import { FaWallet } from "react-icons/fa";
 
 function Header() {
   return (
-    <div>
+    <div className="">
       <>
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link href="#pricing">Pricingtow</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link href="#pricing">Pricingthree</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Navbar collapseOnSelect expand="lg" className="bg-black NavUpperMain">
+          <Container className="">
+            <Navbar.Brand className="text-white" href="#home">React</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto firstNavLink">
+                
+                <Nav.Link className="MainLink" href="#features">Metaverse</Nav.Link>
+                <Nav.Link className="MainLink" href="#pricing">Token</Nav.Link>
+                <Nav.Link className="MainLink" href="#pricing">Quest</Nav.Link>
+                <Nav.Link className="MainLink" href="#pricing">OharaiNFT</Nav.Link>
+                <Nav.Link className="MainLink" href="#pricing">Scholarship</Nav.Link>
+                <Nav.Link className="MainLink" href="#pricing">Dock</Nav.Link>
+                
+              </Nav>
+
+              <Nav className="align-items-center">
+                <Nav.Link href="#deets">
+                  <Form.Select aria-label="Default select example" className="selt_main">
+                    <option>English</option>
+                    <option value="1">Japanese</option>
+                  </Form.Select>
+                </Nav.Link>
+                <Nav.Link className="walletConnect" href="#"><FaWallet className="wallet"/>Connect wallet</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
