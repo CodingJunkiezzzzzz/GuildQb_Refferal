a03f1c6ea85b2f14b6cc253ee94134aecde6fcef
DevNawazishali <alinawazish150p@gmail.com>
2023-07-19 17:45:03 +0500

    first day work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Already_ref from './Components/Already_ref/Already_ref';
+import FAQ from './Components/FAQ/FAQ';
 import Header from './Components/Header/Header';
+import How_it_work from './Components/How_it_work/How_it_work';
+import How_it_work_two from './Components/How_it_work_two/How_it_work_two';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      {/* <How_it_work/> */}
+      {/* <How_it_work_two/> */}
+      <FAQ/>
     </div>
   );
 }
