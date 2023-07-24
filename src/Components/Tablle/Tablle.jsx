49f1883db586b913ddcf0ce459a69fcf6694d0ee
BetterLogicTeam <103257907+HXN97jng@users.noreply.github.com>
2023-07-24 17:00:15 +0500

    Haider monday Merge

--- src/Components/Tablle/Tablle.jsx
+++ src/Components/Tablle/Tablle.jsx
@@ -14,25 +14,25 @@ function Tablle() {
     
         <div className="col-3  col-md-3 col-lg-2  p-3 tableh1 test"> {t('Level')} </div>
         <div className="col-3  col-md-3 col-lg-2 tableborder p-3  tableh1 tpbg">{t('Point')} </div>
-        <div className="col-5  col-lg-2 tableborder brids1 test1 p-3 tableh1 ">{t('Rewads')}</div> </div>
+        <div className="col-5  col-lg-4 tableborder brids1 test1 p-3 tableh1 ">{t('Rewads')}</div> </div>
 
     <div className="row  justify-content-center">
 
         <div className="col-3  col-md-3 col-lg-2   tableborder p-3 tableP"> 1 </div>
         <div className="col-3  col-md-3 col-lg-2  tableborder p-3 tableP">2 </div>
-        <div className="col-5  col-lg-2 tableborder p-3 tableP">QB {t('Badge')} I</div>
+        <div className="col-5  col-lg-4 tableborder p-3 tableP">QB {t('Badge')} I</div>
     </div>
     <div className="row  justify-content-center">
 
         <div className="col-3  ol-md-3 col-lg-2  tableborder p-3 tableP tableP"> 2 </div>
         <div className="col-3  ol-md-3 col-lg-2   tableborder p-3 tableP tableP">2 </div>
-        <div className="col-5  col-lg-2  tableborder p-3 tableP tableP">QB {t('Badge')}  II</div>
+        <div className="col-5  col-lg-4  tableborder p-3 tableP tableP">QB {t('Badge')}  II</div>
     </div>
     <div className="row  justify-content-center">
 
         <div className="col-3  ol-md-3 col-lg-2  tableborder p-3 brids2  tableP tableP test2 "> 3 </div>
         <div className="col-3 ol-md-3 col-lg-2   tableborder p-3 tableP tableP">7~ </div>
-        <div className="col-5 col-lg-2 tableborder p-3 brids3 tableP tableP test3">QB {t('Badge')} III</div>
+        <div className="col-5 col-lg-4 tableborder p-3 brids3 tableP tableP test3">QB {t('Badge')} III</div>
     </div>
 
    </div>
